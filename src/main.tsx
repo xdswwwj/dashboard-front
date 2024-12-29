d709bb3341b7e98d83016c5fa486700eb9f1b612
xdswwwj <xdswwwj@knou.ac.kr>
2024-12-30 01:48:11 +0900

    tailwind css 적용 및 shadcn/ui 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { ReactQueryProvider } from "./providers/ReactQueryProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
