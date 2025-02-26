dd23f910eaa343c70b84c76bdc73848c4fd7fc51
xdswwwj <xdswwwj@knou.ac.kr>
2025-02-27 02:05:00 +0900

    env 제거

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,11 +1,11 @@
+export const VITE_DEV_BASE_URL = "http://localhost:5173";
+export const VITE_PROD_BASE_URL = "https://sanirang.kr";
+export const VITE_DEV_API_URL = "http://localhost:3000";
+export const VITE_PROD_API_URL = "https://api.sanirang.kr";
+
 export const VITE_ENV = import.meta.env;
-console.log("VITE_ENV >>", VITE_ENV);
 export const MODE = VITE_ENV.MODE;
 export const BASE_URL =
-  MODE === "development"
-    ? VITE_ENV.VITE_DEV_BASE_URL
-    : VITE_ENV.VITE_PROD_BASE_URL;
+  MODE === "development" ? VITE_DEV_BASE_URL : VITE_PROD_BASE_URL;
 export const API_BASE_URL =
-  MODE === "development"
-    ? VITE_ENV.VITE_DEV_API_URL
-    : VITE_ENV.VITE_PROD_API_URL;
+  MODE === "development" ? VITE_DEV_API_URL : VITE_PROD_API_URL;
