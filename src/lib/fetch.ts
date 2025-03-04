d677769c6db90c44d9968550e896c2430bc64b32
xdswwwj <xdswwwj@knou.ac.kr>
2025-03-05 08:02:14 +0900

    fetcher 사용 안하는거 제거

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,32 +1,5 @@
 import { API_BASE_URL } from "./config";
 
-export const fetcher = async (
-  endpoint: string,
-  options: RequestInit = {},
-  token?: string | null
-): Promise<any> => {
-  try {
-    const response = await fetch(`${API_BASE_URL}${endpoint}`, {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token || ""}`,
-      },
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.log("errorData >>", errorData);
-      throw new Error(errorData.message || "API 요청 실패");
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error("API Error:", error);
-    throw error;
-  }
-};
-
 export const authFetcher = async (
   endpoint: string,
   options: RequestInit = {},
@@ -43,6 +16,7 @@ export const authFetcher = async (
 
     if (!response.ok) {
       const errorData = await response.json();
+      console.log("errorData >>", errorData);
       throw new Error(errorData.message || "API 요청 실패");
     }
 
